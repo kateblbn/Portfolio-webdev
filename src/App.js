117874fd9382178ecbaf40ea4dfda5aa263817bd
Kate <kate.pidoni@gmail.com>
2025-02-01 14:14:11 +0100

    Deploy My Portfolio to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./assets/templates/Home";
+import About from "./assets/templates/About";
+import Contact from "./assets/templates/Contact";
+import Works from "./assets/templates/Works";
+import 'animate.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="container">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path='/works' element={<Works />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/templates/About.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import last from '../img/icons/last.png'
+import next from '../img/icons/next.png'
+import html from '../img/icons/icons8-html-50.png'
+import css from '../img/icons/icons8-css-50.png'
+import js from '../img/icons/icons8-javascript-50.png'
+import node from '../img/icons/icons8-node-js-96.png'
+import php from '../img/icons/icons8-php-50.png'
+import react from '../img/icons/icons8-react-48.png'
+import sql from '../img/icons/icons8-sql-100.png'
+import sass from '../img/icons/icons8-sass-50.png'
+import Nav from './Nav'
+
+
+function About() {
+  return (
+    <div className='about__wrapper'>
+      <Nav />
+      <div className='non-formal__joke animateCont'>
+        <p >"How do I know if you are a developer?" <br />
+          You spend more time debugging than developing 😄 </p>
+      </div>
+      <h2 className='about__title animateLet'> I'm a <span className='title-bold under'>Web-developer</span> based in Oslo, Norway</h2>
+      <p className='about__descr animateMy'>I do what I love and It's inspires me.</p>
+      <div className='about__skills-wrap'>
+        <h3 className='about__skills-title animatelink1'>My Skills</h3>
+        <p className='about__skills-descr animatelink2'>I have a diverce set of skills, randing from design, to : </p>
+        <div className='skills__wrapper wow animate__animated animate__backInLeft animate__delay-2s'>
+          <div className='skills__wrap wow animateHtml  '>HTML
+            <img className='skills__wrap-img' src={html} alt='html' />
+          </div>
+          <div className='skills__wrap animateCss '>CSS
+            <img className='skills__wrap-img' src={css} alt='css' />
+          </div>
+          <div className='skills__wrap animateJs'>JavaScript
+            <img className='skills__wrap-img' 
+            src={js} alt='js' />
+          </div>
+          <div className='skills__wrap animatePhp'>PHP
+            <img className='skills__wrap-img' src={php} alt='php' />
+          </div>
+          <div className='skills__wrap animateReact'>React js
+            <img className='skills__wrap-img' src={react} alt='react' />
+          </div>
+          <div className='skills__wrap animateNode'>Node Js
+            <img className='skills__wrap-img' src={node} alt='node' />
+          </div>
+          <div className='skills__wrap animateSass'>SASS/SCSS
+            <img className='skills__wrap-img' src={sass} alt='sass' />
+          </div>
+          <div className='skills__wrap animateSql'>SQL
+            <img className='skills__wrap-img' src={sql} alt='sql' />
+          </div>
+        </div>
+        <div>
+          <p className='skills-plan wow animate__animated animate__backInLeft animate__delay-5s'>By working as a team we can achieve success. Your plan, my persistence.</p>
+        </div>
+
+
+      </div>
+      <div className='about__link-wrap'>
+        <div className='about__link'>
+          <Link className='link-last' to='/'>
+            <div className='wrap_link-img'>
+              <img className='about__link-img ' src={last} alt='last' />
+              <div className='link-next-about link-next-last'> Home</div>
+            </div>
+
+          </Link>
+        </div>
+        <div className='about__link'>
+          <Link className='link-next' to='/works'>
+            <div className='wrap_link-img'>
+              <div className='link-next-about link-next-last'> Works</div>
+              <img className='about__link-img' src={next} alt='next' />
+            </div>
+
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
+
+
+
+
+
+// function About() {
+//   return (
+//     <div className='about__wrapper'>
+//       <Nav />
+//       <div className='non-formal__joke wow animate__animated animate__backInLeft animate__delay-0.5s '>
+//         <p >"How do I know if you are a developer?" <br />
+//           You spend more time debugging than developing 😄 </p>
+//       </div>
+//       <h2 className='about__title wow animate__animated animate__backInLeft animate__delay-1s'> I'm a <span className='title-bold under'>Web-developer</span> based in Oslo, Norway</h2>
+//       <p className='about__descr wow animate__animated animate__backInLeft animate__delay-2s'>I do what I love and It's inspires me.</p>
+//       <div className='about__skills-wrap'>
+//         <h3 className='about__skills-title wow animate__animated animate__backInLeft animate__delay-3s'>My Skills</h3>
+//         <p className='about__skills-descr wow animate__animated animate__backInLeft animate__delay-4s'>I have a diverce set of skills, randing from design, to : </p>
+//         <div className='skills__wrapper wow animate__animated animate__backInLeft animate__delay-5s'>
+//           <div className='skills__wrap wow animateHtml  '>HTML
+//             <img className='skills__wrap-img' src={html} alt='html' />
+//           </div>
+//           <div className='skills__wrap animateCss '>CSS
+//             <img className='skills__wrap-img' src={css} alt='css' />
+//           </div>
+//           <div className='skills__wrap animateJs'>JavaScript
+//             <img className='skills__wrap-img' 
+//             src={js} alt='js' />
+//           </div>
+//           <div className='skills__wrap animatePhp'>PHP
+//             <img className='skills__wrap-img' src={php} alt='php' />
+//           </div>
+//           <div className='skills__wrap animateReact'>React js
+//             <img className='skills__wrap-img' src={react} alt='react' />
+//           </div>
+//           <div className='skills__wrap animateNode'>Node Js
+//             <img className='skills__wrap-img' src={node} alt='node' />
+//           </div>
+//           <div className='skills__wrap animateSass'>SASS/SCSS
+//             <img className='skills__wrap-img' src={sass} alt='sass' />
+//           </div>
+//           <div className='skills__wrap animateSql'>SQL
+//             <img className='skills__wrap-img' src={sql} alt='sql' />
+//           </div>
+//         </div>
+//         <div>
+//           <p className='skills-plan wow animate__animated animate__backInLeft animate__delay-5s'>By working as a team we can achieve success. Your plan, my persistence.</p>
+//         </div>
+
+
+//       </div>
+//       <div className='about__link-wrap'>
+//         <div className='about__link'>
+//           <Link className='link-last' to='/'>
+//             <div className='wrap_link-img'>
+//               <img className='about__link-img ' src={last} alt='last' />
+//               <div className='link-next-about link-next-last'> Home</div>
+//             </div>
+
+//           </Link>
+//         </div>
+//         <div className='about__link'>
+//           <Link className='link-next' to='/works'>
+//             <div className='wrap_link-img'>
+//               <div className='link-next-about link-next-last'> Works</div>
+//               <img className='about__link-img' src={next} alt='next' />
+//             </div>
+
+//           </Link>
+
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
